4590390e56d3e55160c6354781d68e1a81fd628d
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-29 16:21:46 -0700

    Added index.handlebars

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express'),
-path = require('path'),
- logger = require('./middleware/logger'),
- posts = require('./Posts'),
+const express = require("express"),
+  path = require("path"),
+  logger = require("./middleware/logger"),
+  posts = require("./Posts"),
+  app = new express(),
+  PORT = process.env.PORT || 80;
 
- app = new express()
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
-app.engine('handlebars', exphbs( {defaultLayout: 'main'}))
+app.set("view engine", "handlebars");
 
-app.set('view engine', 'handlebars')
+app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json)
-app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Command Library",
+    posts,
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("App listening on port 3000")
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/posts", require("./routes/api/posts"));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
