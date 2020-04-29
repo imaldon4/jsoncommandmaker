fc8d771e78768d68f376dae9d413ea8651834e43
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-29 15:59:51 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var commands = [
-    
-]
+const express = require('express'),
+path = require('path'),
+ logger = require('./middleware/logger'),
+ posts = require('./Posts'),
+
+ app = new express()
+
+app.engine('handlebars', exphbs( {defaultLayout: 'main'}))
+
+app.set('view engine', 'handlebars')
+
+app.use(express.json)
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'))
+
+app.listen(3000,()=>{
+    console.log("App listening on port 3000")
+})
